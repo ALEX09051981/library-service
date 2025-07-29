34f123c76b18b0f785499dcd3d8b66b8bb063db5
Alexey <alopar632@gmail.com>
2025-07-29 08:02:20 +0300

    Create custom authentication class with 'Authorize' header support

---
+++ users/authentication.py
@@ -0,0 +1,8 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+class CustomJWTAuthentication(JWTAuthentication):
+    def get_header(self, request):
+        header = request.headers.get("Authorize")
+        if header is None:
+            return None
+        return header.encode("utf-8")
