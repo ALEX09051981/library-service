684d11ab0023eeeb14aafcbbe38ef1624ca85a0a
Alexey <alopar632@gmail.com>
2025-07-29 15:57:03 +0300

    added tests

--- library_service/celery.py
+++ library_service/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_service.settings')
 
-app = Celery('your_project_name')
+app = Celery('library_service')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
