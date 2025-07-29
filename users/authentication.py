bc5269b0b87403cd61e3788f3abcf933c19d04a6
Alexey <alopar632@gmail.com>
2025-07-29 10:44:57 +0300

    Update authentication.py

--- users/authentication.py
+++ users/authentication.py
@@ -2,7 +2,7 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CustomJWTAuthentication(JWTAuthentication):
     def get_header(self, request):
-        header = request.headers.get("Authorize")
+        header = request.headers.get("Authorization")
         if header is None:
             return None
         return header.encode("utf-8")
