8d302008e04cc3fb44fc6decb420c14087aad8fc
Alexey <alopar632@gmail.com>
2025-07-29 13:00:44 +0300

    Display related payments in BorrowingSerializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 from .models import Borrowing
+from payments.models import Payment
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
     is_active = serializers.ReadOnlyField()
+    payment_url = serializers.SerializerMethodField()
+    payment_amount = serializers.SerializerMethodField()
 
     class Meta:
         model = Borrowing
         fields = "__all__"
         read_only_fields = ("user", "borrow_date", "is_active")
+
+    def get_payment_url(self, obj):
+        payment = Payment.objects.filter(borrowing=obj).first()
+        return payment.session_url if payment else None
+
+    def get_payment_amount(self, obj):
+        payment = Payment.objects.filter(borrowing=obj).first()
+        return payment.amount if payment else None
 
 
 class BorrowingReturnSerializer(serializers.ModelSerializer):
