7bc71f367a5597a72572dc08861a0adf0aed054a
Alexey <alopar632@gmail.com>
2025-07-29 08:33:02 +0300

    Created 'me' endpoint, registered route, and added tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserAuthTests(APITestCase):
+    def setUp(self):
+        self.register_url = reverse("register")
+        self.token_url = reverse("token_obtain_pair")
+        self.me_url = reverse("me")
+        self.user_data = {
+            "email": "test@example.com",
+            "password": "strongpassword123"
+        }
+
+    def test_register_user(self):
+        response = self.client.post(self.register_url, self.user_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(email=self.user_data["email"]).exists())
+
+    def test_login_user_and_get_token(self):
+        User.objects.create_user(**self.user_data)
+        response = self.client.post(self.token_url, self.user_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
+        self.assertIn("refresh", response.data)
+
+    def test_access_protected_view_without_token(self):
+        response = self.client.get(self.me_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_access_protected_view_with_token_authorize_header(self):
+        User.objects.create_user(**self.user_data)
+        token_response = self.client.post(self.token_url, self.user_data, format="json")
+        access_token = token_response.data["access"]
+
+        self.client.credentials(HTTP_AUTHORIZE=f"Bearer {access_token}")
+        response = self.client.get(self.me_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["email"], self.user_data["email"])
--- users/views.py
+++ users/views.py
@@ -1,7 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework.response import Response
 from .serializers import UserRegisterSerializer
-from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [permissions.AllowAny]
+
+
+class MeView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        return Response({"email": request.user.email})
